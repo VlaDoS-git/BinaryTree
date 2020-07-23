0c0af869fb637bed0f7fbed6c95ad56a70dbcac7
V_LaDo_S <vladoshorobets2001a@gmail.com>
2020-07-23 15:49:23 +0300

    Fixed

--- BinaryTree/Source.cpp
+++ BinaryTree/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+using namespace std;
+/************************************************************************************************************/
 template<class T>
 class Branch
 {
@@ -7,6 +8,7 @@ public:
 	T data;
 	Branch* left;
 	Branch* right;
+	Branch() = default;
 	Branch(T data = T(),
 		   Branch* left = nullptr,
 		   Branch* right = nullptr);
@@ -21,11 +23,17 @@ class BinaryTree
 public:
 	BinaryTree(void);
 	void insert(T data);
+	const T min(void) const;
+	const T max(void) const;
+	void inOrder(void) const;
 private:
 	Branch<T>* root;
 	size_t _size;
 protected:
 	void insert(T data, Branch<T>* leaf);
+	const Branch<T>* min(Branch<T>* leaf) const;
+	const Branch<T>* max(Branch<T>* leaf) const;
+	void inOrder(Branch<T>* leaf) const;
 };
 /************************************************************************************************************/
 template<class T>
@@ -35,7 +43,26 @@ template<class T>
 void BinaryTree<T>::insert(T data)
 {
 	if (this->root != nullptr){insert(data, this->root);}
-	else {this->root = new Branch(data);}
+	else {this->root = new Branch<T>(data);}
+}
+/************************************************************************************************************/
+template<class T>
+const T BinaryTree<T>::min(void) const
+{
+	if (this->root != nullptr) return min(this->root)->data;
+	return T();
+}
+template<class T>
+const T BinaryTree<T>::max(void) const
+{
+	if (this->root != nullptr) return max(this->root)->data;
+	return T();
+}
+/************************************************************************************************************/
+template<class T>
+void BinaryTree<T>::inOrder(void) const
+{
+	inOrder(this->root);
 }
 /************************************************************************************************************/
 template<class T>
@@ -49,10 +76,10 @@ void BinaryTree<T>::insert(T data, Branch<T>* leaf)
 		}
 		else
 		{
-			leaf->left = new Branch(data);
+			leaf->left = new Branch<T>(data);
 		}
 	}
-	else if(data >= leaf->data)
+	else
 	{
 		if (leaf->right != nullptr)
 		{
@@ -60,15 +87,45 @@ void BinaryTree<T>::insert(T data, Branch<T>* leaf)
 		}
 		else
 		{
-			leaf->right = new Branch(data);
+			leaf->right = new Branch<T>(data);
 		}
 	}
-	
+}
+/************************************************************************************************************/
+template<class T>
+const Branch<T>* BinaryTree<T>::min(Branch<T>* leaf) const
+{
+	if (leaf->left != nullptr) return min(leaf->left);
+	else return leaf;
+}
+/************************************************************************************************************/
+template<class T>
+const Branch<T>* BinaryTree<T>::max(Branch<T>* leaf) const
+{
+	if (leaf->right != nullptr) return max(leaf->right);
+	else return leaf;
+}
+/************************************************************************************************************/
+template<class T>
+void BinaryTree<T>::inOrder(Branch<T>* leaf) const
+{
+	if (leaf != nullptr)
+	{
+		cout << leaf->data << " ";
+		inOrder(leaf->left);
+		inOrder(leaf->right);
+	}
 }
 /************************************************************************************************************/
 int main(int argc, char**argv[])
 {
-
+	BinaryTree<int>arr;
+	arr.insert(40);
+	for (int i = 0; i <= 100; ++i) arr.insert(i);
+	cout << endl;
+	arr.inOrder();
+	cout << endl;
+	cout << endl << arr.max() << " " << arr.min() << endl;
 }
 
 
